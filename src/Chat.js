128f8e7d9cb56024dac37a223dc341d364ba8789
Daniel <floricela999@gmail.com>
2024-01-31 21:48:38 +0100

    llaqi

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,28 @@
+import { Avatar, IconButton } from '@mui/material'
 import './Chat.css'
+import { DonutLarge, MoreVert } from '@mui/icons-material'
 function Chat() {
     return (
         <div className='chat'>
             <div className='chat__header'>
+              <Avatar src={'https://api.dicebear.com/7.x/adventurer/svg?seed=Midnight'} />
 
+              <div className='chat__headerInfo'>
+                <h3>Room name</h3>
+                <p>Last seen at ...</p>
+              </div>
+              <div className='chat__headerRight'>
+              <IconButton>
+                        <DonutLarge />
+                    </IconButton>
+                    <IconButton>
+                        <Chat />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+              </div>
             </div>
-
             <div className='chat__body'>
 
             </div>
